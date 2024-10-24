cc830d3acfd3ef81fd8937684786b1eaf32a89a5
Ghalai <443200504@student.ksu.edu.sa>
2024-10-24 12:28:57 +0300

    Update Create_Truck_2.dart

--- lib/screens/Create_Truck_2.dart
+++ lib/screens/Create_Truck_2.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart'; // Import Firestore
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:tracki/Utils/constants.dart';
 import 'Create_Truck_3.dart';
 
-class CreateTruck2 extends StatelessWidget {
+class CreateTruck2 extends StatefulWidget {
   final String ownerId; // Capture the owner ID
   final String truckName; // Truck name
   final String businessLogo; // Business logo URL
@@ -11,12 +12,6 @@ class CreateTruck2 extends StatelessWidget {
   final String selectedCategory; // Selected category
   final String description; // Description
   final String operatingHours; // Combined operating hours
-
-  // TextEditingController for the location input
-  final TextEditingController locationController = TextEditingController();
-
-  // Create a GlobalKey for the Form
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   CreateTruck2({
     Key? key,
@@ -29,46 +24,49 @@ class CreateTruck2 extends StatelessWidget {
     required this.operatingHours, // Pass combined operating hours
   }) : super(key: key);
 
+  @override
+  _CreateTruck2State createState() => _CreateTruck2State();
+}
+
+class _CreateTruck2State extends State<CreateTruck2> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  LatLng _selectedLocation = LatLng(24.7136, 46.6753); // Default location
+  late GoogleMapController _mapController;
+
   Future<void> _saveTruckDetails(BuildContext context) async {
-    // Validate the form
     if (_formKey.currentState?.validate() ?? false) {
-      // Reference to Firestore collection
       CollectionReference trucks =
           FirebaseFirestore.instance.collection('Food_Truck');
 
       // Create truck data to save
       Map<String, dynamic> truckData = {
-        'name': truckName,
-        'businessLogo': businessLogo,
-        'truckImage': truckImage,
-        'category': selectedCategory,
-        'description': description,
-        'operatingHours': operatingHours,
-        'ownerID': ownerId,
-        'location': locationController.text,
+        'name': widget.truckName,
+        'businessLogo': widget.businessLogo,
+        'truckImage': widget.truckImage,
+        'category': widget.selectedCategory,
+        'description': widget.description,
+        'operatingHours': widget.operatingHours,
+        'ownerID': widget.ownerId,
+        'location':
+            '${_selectedLocation.latitude},${_selectedLocation.longitude}', // Combined string
         'rating': '0',
-        'ratingsCount': 0, //انا غيرتها ساره غ
-        'item_names_list': [], // Initialize with an empty list
-        'item_prices_list': [], // Initialize with an empty list
-        'item_images_list': [], // Initialize with an empty list
+        'ratingsCount': 0,
+        'item_names_list': [],
+        'item_prices_list': [],
+        'item_images_list': [],
       };
 
       try {
-        // Add the truck to Firestore and get the document reference
         DocumentReference truckRef = await trucks.add(truckData);
-
-        // Get the auto-generated ID
         String truckId = truckRef.id;
 
         print("Truck details saved successfully with ID: $truckId");
 
-        // Navigate to CreateTruck3 and pass both the ownerId and truckId
         Navigator.push(
           context,
           MaterialPageRoute(
-            builder: (context) => CreateTruck3(
-                ownerId: ownerId,
-                truckId: truckId), // Pass both ownerId and truckId
+            builder: (context) =>
+                CreateTruck3(ownerId: widget.ownerId, truckId: truckId),
           ),
         );
       } catch (e) {
@@ -77,19 +75,20 @@ class CreateTruck2 extends StatelessWidget {
     }
   }
 
+  void _onMapCreated(GoogleMapController controller) {
+    _mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kBannerColor,
       appBar: AppBar(
-        backgroundColor: const Color(0xFF674188), // Same app bar color
+        backgroundColor: const Color(0xFF674188),
       ),
       body: Column(
         children: [
-          const Expanded(
-            flex: 1,
-            child: SizedBox(height: 5), // Reduced height
-          ),
+          const Expanded(flex: 1, child: SizedBox(height: 5)),
           Expanded(
             flex: 7,
             child: Container(
@@ -103,91 +102,58 @@ class CreateTruck2 extends StatelessWidget {
               ),
               child: SingleChildScrollView(
                 child: Form(
-                  // Wrap with Form
-                  key: _formKey, // Assign the GlobalKey
+                  key: _formKey,
                   child: Column(
                     children: [
                       const Text(
-                        'تحديد الموقع',
+                        'تحديد موقع العربة',
                         style: TextStyle(
                           fontSize: 27.0,
                           fontWeight: FontWeight.w600,
                           color: Color(0xFF674188),
                         ),
-                        textAlign: TextAlign.center, // Align text to the center
+                        textAlign: TextAlign.center,
                       ),
-                      const SizedBox(height: 20.0), // Adjusted space
+                      const SizedBox(height: 20.0),
 
-                      // Location input field
-                      Directionality(
-                        textDirection:
-                            TextDirection.rtl, // Set text direction to RTL
-                        child: TextFormField(
-                          controller: locationController,
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'الرجاء إدخال الموقع'; // Error message in Arabic
-                            }
-                            return null;
-                          },
-                          textAlign: TextAlign.right, // Align text to the right
-                          decoration: InputDecoration(
-                            label: const Text('أدخل الموقع',
-                                textAlign: TextAlign.center),
-                            hintText: 'ادخل الموقع',
-                            hintStyle: const TextStyle(color: Colors.black26),
-                            border: OutlineInputBorder(
-                              borderSide:
-                                  const BorderSide(color: Colors.black12),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide:
-                                  const BorderSide(color: Colors.black12),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            alignLabelWithHint: true,
-                          ),
-                        ),
-                      ),
-
-                      const SizedBox(height: 20.0), // Space after the TextField
-
-                      // Google API Container (Placeholder)
+                      // Google Maps Container
                       Container(
                         height: 400,
                         width: double.infinity,
-                        decoration: BoxDecoration(
-                          color: const Color.fromARGB(255, 247, 134, 158),
-                          border: Border.all(
-                            color: const Color.fromARGB(255, 231, 233, 235),
-                            width: 2.0, // Border width
+                        child: GoogleMap(
+                          onMapCreated: _onMapCreated,
+                          initialCameraPosition: CameraPosition(
+                            target: _selectedLocation,
+                            zoom: 14,
                           ),
-                          borderRadius: BorderRadius.circular(15.0),
-                        ),
-                        alignment: Alignment.center,
-                        child: const Text(
-                          'Here Will Be The Location API',
-                          style: TextStyle(color: Colors.white, fontSize: 18),
-                          textAlign: TextAlign.center, // Center align text
+                          onTap: (LatLng location) {
+                            setState(() {
+                              _selectedLocation =
+                                  location; // Update selected location
+                            });
+                          },
+                          markers: {
+                            Marker(
+                              markerId: MarkerId('selected_location'),
+                              position: _selectedLocation,
+                            ),
+                          },
                         ),
                       ),
 
-                      const SizedBox(height: 30.0), // Spacing before the button
+                      const SizedBox(height: 30.0),
 
                       // Button
                       SizedBox(
                         width: double.infinity,
                         child: ElevatedButton(
                           onPressed: () {
-                            _saveTruckDetails(
-                                context); // Call the function to save details with context
+                            _saveTruckDetails(context);
                           },
                           child: const Text('التالي'), // "Next" button text
-                          // المفروض هنا ينرسل الطلب للأدمن داشبورد وينتظر اليوزر موافقة
                         ),
                       ),
-                      const SizedBox(height: 20.0), // Spacing after the button
+                      const SizedBox(height: 20.0),
                     ],
                   ),
                 ),
