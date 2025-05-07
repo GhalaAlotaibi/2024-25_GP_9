ac06ee2c35209e2cdc47a7caa26cf9c583c580c1
SaraKSSU <sarasyo2003@gmail.com>
2025-05-07 05:01:39 +0300

    Turning the Recommender into a package
    to undo this just delete the __init__ file and check the app.py comment

--- Recommender/app.py
+++ Recommender/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
-from recTest import recommend_food_trucks  # Import the recommendation function
+from Recommender.recTest import recommend_food_trucks
+#change number 1 => was :from recTest import recommend_food_trucks
  
 
 app = Flask(__name__)
